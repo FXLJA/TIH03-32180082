7ac1000984a0c5f5b353f4123d9645d982fb6db9
FXLJA <ja669543@gmail.com>
2021-05-20 14:54:43 +0700

    modul 02

---
+++ Modul_02/Modul_02a/Assets/controlScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlScript : MonoBehaviour
+{
+	private Animator animate;
+
+	// Start is called before the first frame update 
+	void Start()
+	{
+		animate = GetComponent<Animator>();
+	}
+
+	public void walkButton()
+	{
+		animate.Play("walk", -1, 0f);
+	}
+
+	public void runButton()
+	{
+		animate.Play("running", -1, 0f);
+	}
+
+	public void jumpButton()
+	{
+		animate.Play("hit03", -1, 0f);
+	}
+
+	public void rollFButton()
+	{
+		animate.Play("jump", -1, 0f);
+	}
+
+	public void rollBButton()
+	{
+		animate.Play("hit02", -1, 0f);
+	}
+
+	public void kickButton()
+	{
+		animate.Play("hit01", -1, 0f);
+	}
+
+	public void QuitAR()
+	{
+		Application.Quit(); Debug.Log("Exit");
+	}
+}
---
+++ Modul_02/Modul_02b/Assets/controlScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlScript : MonoBehaviour
+{
+    private Animator animate; 
+    // Start is called before the first frame update 
+    void Start() 
+    { 
+        animate = GetComponent<Animator>(); 
+    } 
+
+    public void walkForward() 
+    { 
+        animate.Play("WALK00_F", -1, 0f); 
+    } 
+
+    public void walkBackward() 
+    { 
+        animate.Play("WALK00_B", -1, 0f); 
+    }
+
+    public void walkRight() 
+    { 
+        animate.Play("WALK00_R", -1, 0f); 
+    } 
+
+    public void walkLeft() 
+    { 
+        animate.Play("WALK00_L", -1, 0f); 
+    }
+
+    public void jumpA() 
+    { 
+        animate.Play("JUMP00", -1, 0f); 
+    }
+
+    public void jumpB() 
+    { 
+        animate.Play("JUMP01", -1, 0f); 
+    } 
+
+    public void runForward() 
+    { 
+        animate.Play("RUN00_F", -1, 0f); 
+    } 
+
+    public void runSlide() 
+    { 
+        animate.Play("SLIDE00", -1, 0f); 
+    } 
+
+    public void runRight() 
+    { 
+        animate.Play("RUN00_R", -1, 0f); 
+    } 
+
+    public void runLeft() 
+    { 
+        animate.Play("RUN00_L", -1, 0f); 
+    } 
+
+    public void stopMovement()
+    { 
+        animate.Play("WAIT02", -1, 0f); 
+    }
+}
