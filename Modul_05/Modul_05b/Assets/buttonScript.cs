25ae070798a77edd002e254e3bf02fbc942a6fc5
FXLJA <ja669543@gmail.com>
2021-05-20 15:11:39 +0700

    modul 05

---
+++ Modul_05/Modul_05b/Assets/buttonScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonScript : MonoBehaviour
+{
+    public void SteamURL()
+    {
+        Application.OpenURL("https://steamcommunity.com/id/fxlja1411/");
+    }
+
+    public void DiscordURL()
+    {
+        Application.OpenURL("https://discordapp.com/users/4106");
+    }
+}
