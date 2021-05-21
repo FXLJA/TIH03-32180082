b5610711897cd98da49b2078e14b949f019a9d65
FXLJA <ja669543@gmail.com>
2021-05-21 18:51:41 +0700

    modul 07

---
+++ Modul_07/Modul_07/Assets/ScriptControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptControl : MonoBehaviour
+{
+    private Animator animator; 
+    // Start is called before the first frame update 
+
+    void Start() 
+    { 
+        animator = GetComponent<Animator>(); 
+    } 
+    public void idle() 
+    { 
+        animator.Play("Idle_3", -1, 0f); 
+    } 
+    public void walk() 
+    { 
+        animator.Play("Walk_IP", -1, 0f); 
+    } 
+    public void run() 
+    { 
+        animator.Play("Run_IP", -1, 0f); 
+    } 
+    public void attack() 
+    {
+        animator.Play("Attack_Arm_1", -1, 0f);
+    }
+}
---
+++ Modul_07/Modul_07/Assets/ScriptMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+using UnityEngine.SceneManagement; 
+
+public class ScriptMenu : MonoBehaviour 
+{ 
+    public GameObject ConfirmPanel; 
+    public GameObject AboutPanel;
+
+    public void Exit() 
+    { 
+        Application.Quit(); 
+    }
+
+    public void Play(string scene_name) 
+    {
+        SceneManager.LoadScene(scene_name); 
+    }
+
+    public void ConfirmClicked() 
+    { 
+        ConfirmPanel.SetActive(true); 
+        AboutPanel.SetActive(false); 
+    }
+
+    public void AboutClicked() 
+    {
+        ConfirmPanel.SetActive(false); 
+        AboutPanel.SetActive(true);
+    }
+
+    public void Back()
+    {
+        ConfirmPanel.SetActive(false);
+    }
+}
