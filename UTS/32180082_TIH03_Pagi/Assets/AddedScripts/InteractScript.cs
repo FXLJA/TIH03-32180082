80f20629f14932b3668a550f616afc513db13cc7
FXLJA <ja669543@gmail.com>
2021-05-23 00:40:19 +0700

    uts

---
+++ UTS/32180082_TIH03_Pagi/Assets/AddedScripts/InteractScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractScript : MonoBehaviour
+{
+    public GameObject[] targets;
+    public bool doRotateL 
+    { 
+        get; set; 
+    }
+    public bool doRotateR 
+    { 
+        get; set; 
+    }
+
+    public void ZoomIn()
+    {
+        foreach (GameObject target in targets)
+        {
+            target.transform.localScale = target.transform.localScale * 1.2f;
+        }
+    }
+
+    public void ZoomOut()
+    {
+        foreach (GameObject target in targets)
+        {
+            target.transform.localScale = target.transform.localScale / 1.2f;
+        }
+    }
+
+    public void RotateLeft()
+    {
+        foreach (GameObject target in targets)
+        {
+            target.transform.Rotate(new Vector3(0, 1.5f, 0));
+        }
+    }
+
+    public void RotateRight()
+    {
+        foreach (GameObject target in targets)
+        {
+            target.transform.Rotate(new Vector3(0, -1.5f, 0));
+        }
+    }
+
+    private void Update()
+    {
+        if (doRotateL)
+        {
+            RotateLeft();
+        }
+
+        if (doRotateR)
+        {
+            RotateRight();
+        }
+    }
+}
---
+++ UTS/32180082_TIH03_Pagi/Assets/AddedScripts/ResetTransformation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetTransformation : MonoBehaviour
+{
+    private Vector3 defaultPosition;
+    private Vector3 defaultScale;
+    private Quaternion defaultRotation;
+    void Start()
+    {
+        defaultPosition = gameObject.transform.localPosition;
+        defaultRotation = gameObject.transform.localRotation;
+        defaultScale = gameObject.transform.localScale;
+    }
+
+    public void Reset()
+    {
+        gameObject.transform.localPosition = defaultPosition;
+        gameObject.transform.localRotation = defaultRotation;
+        gameObject.transform.localScale = defaultScale;
+    }
+}
