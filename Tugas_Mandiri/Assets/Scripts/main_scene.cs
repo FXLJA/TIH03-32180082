067be151f81afb1074ac0543227fb8b10d6b3f31
FXLJA <ja669543@gmail.com>
2021-06-07 03:39:07 +0700

    finished tugas_mandiri

---
+++ Tugas_Mandiri/Assets/Scripts/main_scene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class main_scene : MonoBehaviour
+{
+    GameObject ConfirmPanel;
+    private void Start()
+    {
+        ConfirmPanel = GameObject.FindGameObjectsWithTag("HidePanel")[0];
+        ConfirmPanel.SetActive(false);
+    }
+    public void ExitClick()
+    {
+        ConfirmPanel.SetActive(true);
+    }
+    public void NoClick()
+    {
+        ConfirmPanel.SetActive(false);
+        SceneManager.LoadScene(sceneBuildIndex: 0);
+    }
+    public void YesClick()
+    {
+        Application.Quit();
+    }
+}
